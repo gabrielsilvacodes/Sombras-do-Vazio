de97288488404cf609ab35305b5499adfb56c7d8
gabrielsilvacodes <gabrielsilvadeveloper@outlook.com>
2025-06-03 03:14:34 -0300

    title_screen adicionada

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,41 +1,68 @@
 extends CharacterBody2D
 
 const SPEED := 150.0
-const JUMP_FORCE := -400.0
+const AIR_FRICTION := 0.5
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
+var can_jump := true
 var is_hurted := false
 var knockback_vector := Vector2.ZERO
+var knockback_power := 20
 var direction
+
+#handle jump and gravity
+@export var jump_height := 96
+@export var max_time_to_peak := 0.5
+
+var jump_velocity
+var gravity
+var fall_gravity
 
 
 @onready var animation: AnimatedSprite2D = $anim
 @onready var remote_transform := $remote as RemoteTransform2D
+@onready var coyote_timer = $coyote_timer as Timer
 @onready var jump_sfx = $jump_sfx as AudioStreamPlayer
 @onready var hit_sfx = $hit_sfx
+@onready var player_start_position = $"../player_start_position"
+
 
 signal player_has_died()
 
-func _physics_process(delta):
-	# Gravidade
-	if not is_on_floor():
-		velocity.y += gravity * delta
+func _ready():
+	jump_velocity = (jump_height * 2) / max_time_to_peak
+	gravity = (jump_height * 2) / pow(max_time_to_peak, 2)
+	fall_gravity = gravity * 2
 
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.x = 0
+#		velocity.y += gravity * delta
+		
 	# Pulo
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_FORCE
+	if Input.is_action_just_pressed("ui_accept") and can_jump:
+		velocity.y = -jump_velocity
 		is_jumping = true
 		jump_sfx.play()
 	elif is_on_floor():
 		is_jumping = false
+		
+	if velocity.y > 0 or not Input.is_action_pressed("ui_accept"):
+		velocity.y += fall_gravity * delta
+	else:
+		velocity.y += gravity * delta
 	
+	
+	if is_on_floor() and !can_jump:
+		can_jump = true
+	elif can_jump and coyote_timer.is_stopped():
+		coyote_timer.start()
 
 	# Direção horizontal
 	direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction != 0:
-		velocity.x = direction * SPEED
+		velocity.x = lerp(velocity.x, direction * SPEED, AIR_FRICTION)
 		animation.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -54,12 +81,10 @@ func _physics_process(delta):
 
 # Recebendo dano do inimigo
 func _on_hurtbox_body_entered(body):
-	if $ray_right.is_colliding():
-		take_damage(Vector2(-200, -200))
-	elif $ray_left.is_colliding():
-		take_damage(Vector2(200, -200))
+	var knockback = Vector2((global_position.x - body.global_position.x) * knockback_power, -200)
+	print(knockback)
+	take_damage(knockback)
 	
-
 # Seguir a câmera
 func follow_camera(camera):
 	var camera_path = camera.get_path()
@@ -90,14 +115,35 @@ func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 func _set_state():
 	var state = "idle"
 	
-	if !is_on_floor():
-		state = "jump"
+	if is_hurted:
+		state = "hit"
+	elif !is_on_floor():
+		if velocity.y > 0:
+			state = "fall"
+		else:
+			state = "jump"
 	elif direction != 0:
 		state = "run"
 		
-	if is_hurted:
-		state = "hit"
-	
 	if animation.name != state:
 		animation.play(state)
+
 	
+
+func _on_coyote_timer_timeout():
+	can_jump = false
+
+func handle_death_zone():
+	if Globals.player_life > 1:
+		Globals.player_life -= 1
+		visible = false
+		set_physics_process(false)
+		
+		await get_tree().create_timer(1.0).timeout
+		Globals.respawn_player()
+		visible = true
+		set_physics_process(true)
+	else:
+		visible = false
+		await get_tree().create_timer(0.5).timeout
+		player_has_died.emit()
